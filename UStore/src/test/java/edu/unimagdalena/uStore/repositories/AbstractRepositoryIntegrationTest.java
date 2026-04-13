06bf12bb24d8dd65d2e5c96d080aeacb63ab28a1
Camilo Alvarez <capineros@unimagdalena.edu.co>
2026-04-12 20:13:45 -0500

    Nueva implemetnación de AbstractRepositoryIntegrationTest

--- UStore/src/test/java/edu/unimagdalena/uStore/repositories/AbstractRepositoryIntegrationTest.java
+++ UStore/src/test/java/edu/unimagdalena/uStore/repositories/AbstractRepositoryIntegrationTest.java
@@ -1,14 +1,28 @@
 
 package edu.unimagdalena.uStore.repositories;
 
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 @ActiveProfiles("test")
-@Transactional
 public abstract class AbstractRepositoryIntegrationTest{
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:16-alpine");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+    }
 }
