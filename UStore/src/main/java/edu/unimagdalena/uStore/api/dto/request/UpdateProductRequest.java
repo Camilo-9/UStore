ceb8a6fcbbdcd036ca0fe6f6bb16d5c46338cc29
Camilo Alvarez <capineros@unimagdalena.edu.co>
2026-05-25 14:10:40 -0500

    Status agregado a UpdateCustomerRequest y active a UpdateProductRequest

--- UStore/src/main/java/edu/unimagdalena/uStore/api/dto/request/UpdateProductRequest.java
+++ UStore/src/main/java/edu/unimagdalena/uStore/api/dto/request/UpdateProductRequest.java
@@ -20,6 +20,8 @@ public class UpdateProductRequest{
     @NotNull
     private Long categoryId;
 
+    private Boolean active;
+
     public String getName(){
         return name;
     }
@@ -50,5 +52,13 @@ public class UpdateProductRequest{
 
     public void setCategoryId(Long categoryId){
         this.categoryId = categoryId;
+    }
+
+    public Boolean getActive(){
+        return active;
+    }
+
+    public void setActive(Boolean active){
+        this.active = active;
     }
 }
